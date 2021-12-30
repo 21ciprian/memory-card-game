b42eff6f7ef8b8dd2788f15cb8d9bf823c59cdf6
21ciprian <2021ciprianoffice@gmail.com>
2021-12-30 01:23:26 +0000

    Created checkForMatch function

--- main.js
+++ main.js
@@ -55,6 +55,9 @@ const gameGrid = document.querySelector('.grid')
 let cardsChosen = []
 //store the cards ids of the card that was clicked
 let cardChosenId = []
+//store the cards that match in an array
+let cardsMatched = []
+
 
 
 //create board game
@@ -75,6 +78,8 @@ function createBoardGame() {
     gameGrid.appendChild(card)
   })
 }
+
+
 //create flip card function to show images
 function flipCard() {
   //get the id of the card and store it in a variable
@@ -88,6 +93,38 @@ function flipCard() {
   console.log('cardChosenId: ', cardChosenId)
   //set the src attribute to be the img from the cards array tht matches the cardid
   this.setAttribute('src', cardsArray[cardId].img)
+  if (cardsChosen.length === 2) {
+    setTimeout(checkForMatch, 500)
+  }
+}
+
+
+//create function to check if 2 cards are matching
+function checkForMatch() {
+  //select all the cards
+  const cards = document.querySelectorAll('img')
+  //select first card by id from the chosencard id array
+  const optionOneId = cardChosenId[0]
+  //select second card by id from the chosencard id array
+  const optionTwoId = cardChosenId[1]
+  //check if the cards names with the given ids match
+  if (cardsChosen[0] === cardsChosen[1]) {
+    console.log('Well done! Cards match!')
+    //if they match remove them from the board by changing the fruit image with a blank one
+    cards[optionOneId].setAttribute('src', 'images/white.png')
+    cards[optionTwoId].setAttribute('src', 'images/white.png')
+    //store the cards in the cardsMatched array
+    cardsMatched.push(cardsChosen)
+  }
+  else {
+    //if they don't match change the fruit img with qmark img
+    cards[optionOneId].setAttribute('src', 'images/qmark.png')
+    cards[optionTwoId].setAttribute('src', 'images/qmark.png')
+    console.log('Cards do not match! Try again!')
+  }
+  //reset the cardsChosen and cardChosenId after every second card flip
+  cardsChosen = []
+  cardChosenId = []
 
 }
 
