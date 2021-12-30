ec3cb51061cbce70453a5c5f4b9d1ddc3c5f094f
21ciprian <2021ciprianoffice@gmail.com>
2021-12-30 00:13:23 +0000

    created cards array

---
+++ main.js
@@ -0,0 +1,51 @@
+//create cards array for the game
+const cardsArray = [
+  {
+    name: 'apple',
+    img: 'images/apple.jpg'
+  },
+  {
+    name: 'banana',
+    img: 'images/banana.jpg'
+  },
+  {
+    name: 'grape',
+    img: 'images/grape.jpg'
+  },
+  {
+    name: 'kiwi',
+    img: 'images/kiwi.jpg'
+  },
+  {
+    name: 'orange',
+    img: 'images/orange.jpg'
+  },
+  {
+    name: 'pear',
+    img: 'images/pear.jpg'
+  },
+  {
+    name: 'apple',
+    img: 'images/apple.jpg'
+  },
+  {
+    name: 'banana',
+    img: 'images/banana.jpg'
+  },
+  {
+    name: 'grape',
+    img: 'images/grape.jpg'
+  },
+  {
+    name: 'kiwi',
+    img: 'images/kiwi.jpg'
+  },
+  {
+    name: 'orange',
+    img: 'images/orange.jpg'
+  },
+  {
+    name: 'pear',
+    img: 'images/pear.jpg'
+  },
+]
